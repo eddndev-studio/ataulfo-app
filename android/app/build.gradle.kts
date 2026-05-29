997ef1f21145624eac4d8d25bfc070cc99801a73
eddndev <contacto@eddndev.com>
2026-05-29 01:06:27 -0600

    ♻️ rebrand(android): identidad studio.eddndev.ataulfo + label Ataúlfo
    namespace + applicationId studio.eddndev.ataulfo, paquete Kotlin movido a
    .../ataulfo/, label de pantalla Ataúlfo. Verificado con flutter build apk
    --debug (compila Gradle/Kotlin, fuera del alcance de analyze/test).

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "studio.eddndev.agentic"
+    namespace = "studio.eddndev.ataulfo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "studio.eddndev.agentic"
+        applicationId = "studio.eddndev.ataulfo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
